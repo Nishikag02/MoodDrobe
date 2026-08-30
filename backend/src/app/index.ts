c1c145a2f8d7a5a0a70e0ac791d0111d37df20b3
kartik82006 <kartikgupta15244.dcmc@gmail.com>
2026-08-30 08:42:23 +0530

    feat: integrate better-auth for authentication and set up database schema
    - Updated pnpm-workspace.yaml to enable esbuild.
    - Added sign-in controller for handling sign-in requests.
    - Modified index.ts to include authentication middleware using better-auth.
    - Created db/index.ts to set up PostgreSQL connection using drizzle-orm.
    - Added schema definitions for user, session, account, and verification tables in schema.ts.
    - Removed old drizzle index and schema files.
    - Implemented auth-client.ts to create an authentication client.
    - Developed auth.ts to configure better-auth with drizzle adapter and database schema.
    - Updated tsconfig.json to include src/**/*.ts for TypeScript compilation.

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -1,17 +1,20 @@
 import express from 'express'
 import type {Express} from 'express'
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "../lib/auth.js";
 
 export function expressApp() :Express{
     const app = express();
 
     // Middleware
+    app.all('/api/auth/{*any}', toNodeHandler(auth));
 
+    app.use(express.json());
 
     // Routes
     app.get('/', (req, res) => {
         res.json({'message': 'Hello World!'});
     });
-
 
 
     return app;
